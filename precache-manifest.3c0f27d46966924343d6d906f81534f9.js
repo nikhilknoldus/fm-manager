0975ab630ea028cb7bb6cb68e18c4fc387539071
Nikhil Kumar <nikhilkumar.net@gmail.com>
2020-05-12 13:03:42 +0530

    Updates

---
+++ precache-manifest.3c0f27d46966924343d6d906f81534f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27e3a9c091e1999371c1c7c4628e5d3",
+    "url": "/nikhilknoldus/fm-manager/index.html"
+  },
+  {
+    "revision": "e9213ab70ac0f4a80a0e",
+    "url": "/nikhilknoldus/fm-manager/static/css/main.f82d15c0.chunk.css"
+  },
+  {
+    "revision": "b00b7c8bf5ed688b2d86",
+    "url": "/nikhilknoldus/fm-manager/static/js/2.c36e2bc4.chunk.js"
+  },
+  {
+    "revision": "0f5857560c5a39216b31520630cd7416",
+    "url": "/nikhilknoldus/fm-manager/static/js/2.c36e2bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9213ab70ac0f4a80a0e",
+    "url": "/nikhilknoldus/fm-manager/static/js/main.d6000243.chunk.js"
+  },
+  {
+    "revision": "30caf02a3b59a6e636e0",
+    "url": "/nikhilknoldus/fm-manager/static/js/runtime-main.3b762ff4.js"
+  }
+]);
